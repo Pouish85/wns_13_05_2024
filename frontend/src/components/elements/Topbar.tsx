2e4664a53c54994a1bfa33c7ef758443639ff16a
Pouish85 <pouish.iphone@gmail.com>
2024-07-08 10:47:27 +0200

    feat: init front project

---
+++ frontend/src/components/elements/Topbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Topbar = () => {
+
+  return (
+    <div
+      className={`fixed top-0 h-[10%] w-full flex items-center py-4 z-50 bg-primary`}
+    >
+      <div className="flex flex-col w-full items-center text-center justify-center align-middlet">
+        <h1 className="text-white font-bold text-lg">Checkpoint: frontend</h1>
+        <p className="text-white">Countries</p>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Topbar from "@/components/elements/Topbar";
+
 export default function Home() {
-  return <h1>Hello, wilder !</h1>;
+  return (
+      <>
+        <main className="flex">
+          <Topbar />
+          <h1 className="text-red-600 bg-red-500">Hello, wilder !</h1>
+        </main>
+      </>
+  );
 }
